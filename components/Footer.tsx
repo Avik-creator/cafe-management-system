87f773a1c68aed35cfa2b241b7098c7ff969ac42
Subha Sadhu <subhasadhu5@gmail.com>
2024-03-23 22:24:01 +0530

    Minimize Footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ export const Footer = () => {
     <header className="flex h-20 w-full items-center justify-center">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <h1 className="text-2xl font-extrabold text-white tracking-wide">
-            All Rights Reserved
+          <h1 className="text-xs text-white tracking-wide">
+            &#169; All Rights Reserved
           </h1>
         </div>
       </div>
