025bc6e0139e2dbd589d998b435f6b213d53c0eb
Avik-creator <avikm744@gmail.com>
2024-06-11 11:06:38 +0530

    Added Changes.

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  let loggedin = req.cookies.get("access");
-  const { pathname } = req.nextUrl;
+  try {
+    let loggedin = req.cookies.get("access");
 
-  if (loggedin && pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
+    const { pathname } = req.nextUrl;
 
-  if (!loggedin && pathname !== "/") {
-    return NextResponse.redirect(new URL("/", req.url));
+    if (loggedin && pathname === "/") {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+
+    if (!loggedin && pathname !== "/") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } catch (error) {
+    console.error("Error in middleware:", error);
   }
 }
 
