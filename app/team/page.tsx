59aeb63629fb899ccfd1b3a125e7fdcbb5ac953d
Avik-creator <avikm744@gmail.com>
2024-06-21 17:17:42 +0530

    Added Changes.

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -59,14 +59,10 @@ export default function Component() {
                 className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:hover:bg-gray-700"
               >
                 <Image
-                  className={`object-cover w-full rounded-t-lg ${
-                    index === teamMembers.length - 1
-                      ? "h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                      : "h-96 md:w-48 md:rounded-none md:rounded-s-lg"
-                  }`}
+                  className="object-cover rounded-t-lg h-96 md:w-48 md:rounded-none md:rounded-s-lg"
                   src={member.image}
-                  width={600}
-                  height={500}
+                  width={200}
+                  height={200}
                   alt={member.name}
                 />
                 <div className="flex flex-col justify-between p-4 leading-normal">
